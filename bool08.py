4cccbd2cc8d9814e54510ae1fb07b122feb64396
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-25 21:50:23 +0500

    bool08.py

--- bool08.py
+++ bool08.py
@@ -7,6 +7,6 @@ def main(a):
         bool
     """
     # Write your code here
-    return (a % 2 == 1 or a % 2 == 0) or (a>0)
+    return (a % 2 == 1 or a % 2 == 0) or (a>0 or a==0)
 nuber = main(-8.0)
 print(bool(nuber))
